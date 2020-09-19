30a49e605f8590861554e51b83b46dfdea9599b1
Ali Rehmatullah <16440817+LPsyCongroo@users.noreply.github.com>
2020-09-18 21:21:07 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="card">
+      <h1>Virtuous Design</h1>
+      <div className="innerCard">
+        <h2>Projects:</h2>
+      </div>
+    </div>
+  );
+}
